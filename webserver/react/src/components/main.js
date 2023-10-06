7371ef30bed68991099a923d19f5a539a320cfe3
junghanm <maxhenning.junghans@draeger.com>
2023-10-06 21:10:27 +0200

    Quick fix

--- webserver/react/src/components/main.js
+++ webserver/react/src/components/main.js
@@ -1,19 +1,20 @@
-import Searchbar from "./searchbar";
-import ResultBox from "./resultBox";
-import {useState} from "react";
+//import Searchbar from "./searchbar";
+//import ResultBox from "./resultBox";
+//import {useState} from "react";
 
 function Main() {
-    const [api, updateApi] = useState(() => {
-        let temp = window.location.href.replace("http://localhost:3000/", "");
-        temp = temp.replace("site/v1/food/", "");
-        return temp;
-    })
+    //const [api, updateApi] = useState(() => {
+    //    let temp = window.location.href.replace("http://localhost:3000/", "");
+    //    temp = temp.replace("site/v1/food/", "");
+    //    return temp;
+    //})
 
     return (
         <div className="d-flex justify-content-center">
             <div className="d-flex flex-column">
-                <Searchbar update={updateApi}/>
-                <ResultBox url={"http://127.0.0.1:5000/api/v1/food/" + api}/>
+                MOIN!!!
+                //<Searchbar update={updateApi}/>
+                //<ResultBox url={"http://127.0.0.1:5000/api/v1/food/" + api}/>
             </div>
         </div>
     );
