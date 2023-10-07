70fa0b1510ebbdd265e464f0608e24cb06a6f5dd
Wendelin Karg <wendelin.karg@student.kit.edu>
2023-10-07 15:38:57 +0200

    test

--- webserver/react/src/components/Graphs.js
+++ webserver/react/src/components/Graphs.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import {InfluxDB, Point} from '@influxdata/influxdb-client';
 
@@ -9,30 +9,38 @@ function Graphs(props) {
     const org = 'draeger'
     const queryApi = new InfluxDB({url, token}).getQueryApi(org)
 
-    const fluxQuery = `from(bucket: "robomaster")
-  |> range(start: -10m)
-  |> filter(fn: (r) => r["_measurement"] == "sps30")
-  |> filter(fn: (r) => r["_field"] == "mass_pm1")
-  |> yield(name: "mean")`
+    const [x_values, setX] = useState([]);
+    const [y_values, setY] = useState([]);
 
-    var x_values = []
-    var y_values =  []
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+    // Update the document title using the browser API
 
-queryApi.queryRows(fluxQuery, {
-  next: (row, tableMeta) => {
-    const tableObject = tableMeta.toObject(row)
-    x_values.push(tableObject._time)
-    y_values.push(tableObject._value)
-  },
-  error: (error) => {
-    console.error('\nError', error)
-  },
-  complete: () => {
-    console.log('\nSuccess')
-  },
-})
-x_values.map(x => return x.replace("T", " "))
-console.log(x_values)
+      const fluxQuery = `from(bucket: "robomaster")
+      |> range(start: -10m)
+      |> filter(fn: (r) => r["_measurement"] == "sps30")
+      |> filter(fn: (r) => r["_field"] == "mass_pm1")
+      |> yield(name: "mean")`
+
+    var x_temp = []
+    var y_temp = []
+    queryApi.queryRows(fluxQuery, {
+      next: (row, tableMeta) => {
+        const tableObject = tableMeta.toObject(row)
+        x_temp.push(tableObject._time)
+        y_temp.push(tableObject._value)
+      },
+      error: (error) => {
+        console.error('\nError', error)
+      },
+      complete: () => {
+        setX(x_temp)
+        setY(y_temp)
+        console.log('\nSuccess')
+      },
+    })
+    });
+
 
 
     return (
@@ -47,11 +55,7 @@ console.log(x_values)
 				name: 'Test'
 			}
         ]}
-        layout={
-        width: "100%",
-        height: "100%",
-        title: 'A Fancy Plot',
-        } }
+        layout={{width: "100%",height: "100%",title: 'A Fancy Plot'}}
         />
     );
 }
