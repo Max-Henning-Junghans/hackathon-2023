bfe96fcf9e06313fbe6bc194622dcc9fdb3433f2
junghanm <maxhenning.junghans@draeger.com>
2023-10-07 12:37:49 +0200

    Updated graph

--- webserver/react/src/components/Graphs.js
+++ webserver/react/src/components/Graphs.js
@@ -5,20 +5,22 @@ function Graphs(props) {
     return (
         <Plot
         data={[
-		{
-			x: [1, 2, 3, 4, 5, 6],
-			y: [2, 6, 3, 1, 2, 3],
-			type: 'scatter+markers',
-			mode: 'lines',
-			marker: {color: 'red'},
-			name: 'Test'
-		},
-			x: [1, 2, 3, 4, 5, 6],
-			y: [2, 6, 3, 1, 2, 3],
-			type: 'scatter',
-			mode: 'lines',
-			marker: {color: 'green'},
-			name: 'Moin'
+			{
+				x: [1, 2, 3, 4, 5, 6],
+				y: [2, 6, 3, 1, 2, 3],
+				type: 'scatter+markers',
+				mode: 'lines',
+				marker: {color: 'red'},
+				name: 'Test'
+			},
+			{
+				x: [1, 2, 3, 4, 5, 6],
+				y: [2, 6, 3, 1, 2, 3],
+				type: 'scatter',
+				mode: 'lines',
+				marker: {color: 'green'},
+				name: 'Moin'
+			}
         ]}
         layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
         />
